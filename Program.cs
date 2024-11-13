9d7bfd63f79d25348bc27706ea430fa07b1d9c72
Dongpeng Qian <dongpeng.qian@stmartin.edu>
2024-11-13 09:17:18 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MergeAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorting project:");
+            int[] arr = { 2, 1, 5, 4 };
+            Print(arr);
+            int[] a = { 1, 3, 5, 7 };
+            int[] b = { 2, 4, 6, 8 };
+            int[] c = new int[a.Length + b.Length];
+            Sort.Merge(a, b, c);
+            Console.WriteLine("After merging:");
+            Print(c);
+        }
+        // C# code to merge two sorted arrays into a third one
+        
+
+        static void Print(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ sort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeAlgorithm
+{
+    public class Sort
+    {
+        public static void Merge(int[] a, int[] b, int[] c)
+        {// both a and b must sorted, c stores the merged data
+            int x = 0, y = 0, z = 0;
+            // when both sorted arrays have elements, copy the smaller one
+            // to array c, and advance the index of that array
+            while (x < a.Length && y < b.Length)
+            {
+                if (a[x] < b[y])
+                {
+                    c[z++] = a[x++];
+                }
+                else
+                {
+                    c[z++] = b[y++];
+                }
+            }
+            // Copy over remainder of the rest of a to c
+            while (x < a.Length)
+            {
+                c[z++] = a[x++];
+            }
+            // Copy over remainder of the rest of b to c
+            while (y < b.Length)
+            {
+                c[z++] = b[y++];
+            }
+        }
+    }
+}
